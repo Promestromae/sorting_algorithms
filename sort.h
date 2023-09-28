ec3032d13fa878b01d076581233bf15c756c4c54
Promestromae <nzuvameshack@gmail.com>
2023-09-28 16:12:50 +0000

    so frenzy

--- sort.h
+++ sort.h
@@ -37,3 +37,5 @@ void counting_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
+
+#endif /* SORT_H */
